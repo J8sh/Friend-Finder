35e883517464681049c777016a48a0df38c53f12
Josh <j8shigarcia@aol.com>
2017-10-13 17:47:32 -0500

    Project

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo" : "http://img2.looper.com/img/gallery/why-hollywood-wont-cast-josh-radnor-anymore/his-directing-projects-are-taking-too-much-time.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Sherri",
+    "photo" : "http://images.newyorkdress.com/Items_Big/68370_247921.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Kirill",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Kirill_Troussov_26.jpg/1200px-Kirill_Troussov_26.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "http://www.lyricscode.com/wp-content/uploads/2017/08/Amy-Winehouse-4.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gavin",
+    "photo" : "https://pbs.twimg.com/profile_images/926728123/GavinMcInnes.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kayla",
+    "photo" : "http://www.northofhere.com.au/wp-content/uploads/2015/11/northofhere-kayla-itsines.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "PJ",
+    "photo" : "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/03/18/14/pj-harvey.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Janie",
+    "photo" : "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4NTkwMzk4Ml5BMl5BanBnXkFtZTgwMTA0MDQ5MDE@._V1_SY1000_CR0,0,664,1000_AL_.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/5/53/Eric_Andre_%287440656420%29.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Krissy",
+    "photo" : "https://pbs.twimg.com/profile_images/888869917944041472/xsTqEs8W.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Zob",
+    "photo" : "https://pbs.twimg.com/profile_images/732053745408876544/v9zATub3.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+    "photo" : "http://www.thenewpotato.com/wp-content/uploads/2014/12/danielle-campbell-the-originals.jpg?x84488",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Charu",
+    "photo" : "http://www.tellychakkar.com/sites/www.tellychakkar.com/files/styles/display_350x350/public/images/celebs/2013/07/20/Charu_Asopa03.jpg?itok=YrO2V79l",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Omar",
+    "photo" : "https://flowhot.me/wp-content/uploads/2017/01/Don-Omar-1.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
